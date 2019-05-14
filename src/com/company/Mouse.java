d439dbd1d4f27f820e67440c2924c6cc36ca0d90
AlekseyZaychenkov <alexzdes.1989@yandex.ru>
2019-05-14 22:26:02 +0400

    Initial commit

---
+++ src/com/company/Mouse.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * Created by Aleksey Zaychenkov on 14.05.2019.
+ */
+
+public class Mouse {
+    private int x;
+    private int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/com/company/SnakeDirection.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Aleksey Zaychenkov on 14.05.2019.
+ */
+
+public enum SnakeDirection {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+}
+
